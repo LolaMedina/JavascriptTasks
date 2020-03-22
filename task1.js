2d2f214182200337441a97f4f3c61235481ab801
LolaMedina <mlola.oseni@gmail.com>
2020-03-22 13:20:08 +0100

    Completed JS task

---
+++ task1.js
@@ -0,0 +1,15 @@
+
+const myName = 'Damilola Oseni';
+
+let courses = ['HTML', 'CSS', 'Javascript', 'C#'];
+
+console.log(`My name is ${myName}, my courses are: ${courses}`);
+
+
+for (x=0; x<=200; x++)
+{
+   if (x % 2 === 0){
+            console.log(x);
+   }
+};
+
