90f160c4f8847843da00a362f0c7f99b67dc8b65
LolaMedina <56541025+LolaMedina@users.noreply.github.com>
2020-04-19 14:20:55 +0100

    Corrected a mistake.

--- JS-Tasks/task3.js
+++ JS-Tasks/task3.js
@@ -1,37 +1,33 @@
-const animeCheck = num => {
-    let output = [];
-    let word;
+const AnimeCheck = num => {
+    //check if number is an integer
+   if (!Number.isInteger(num)) {
+       throw new Error('Number should be an integer');
+   }
+   //sets array to 0 
+   const output  = [];
+   output.length = 0;
+   
+   for(let i = 1; i <=num; i++){
+       let word = "";
 
-    for(x = 1; x<= num; x++ ){
-        if(x % 2 !== 0 && x % 3 !== 0 && x % 5 !== 0){
-            word = x;
-        }
-        else if((x % 2 === 0) && (x % 3 === 0) && (x % 5 === 0)){
-            word = "yu-gi-oh"
-        }
-        else if ((x % 2 === 0) && (x % 5 === 0)){
-            word = "yu-oh"
-        }
-        else if ((x % 2 === 0) && (x % 3 === 0)){
-            word = "yu-gi"
-        }
-        else if ((x % 3 === 0) && (x % 5 === 0)){
-            word = "gi-oh"
-        }
-        else if(x % 2 === 0 ){
-            word = "yu";
-        }
-      
-        else if (x % 3 === 0){
-            word = "gi"
-        }
-       
-        else {
-            word = "oh"
-        }
-        output.push(word);
-    }
-    return(output);
-}
-console.log(10);
-console.log(animeCheck(100));
+       if(i % 2 === 0) {
+           word += "yu";
+       }
+       if(i % 3 === 0) {
+           word += "-gi";
+       }
+       if(i % 5 === 0){
+           word += "-oh";
+       }
+       if(!word){
+           output.push(i);
+       }
+       else{
+           output.push(word);
+       }
+   }
+   return(output);
+  
+};
+console.log(AnimeCheck(100));
+console.log(AnimeCheck(50));
