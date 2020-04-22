bc276d8ae758f62d3f24da2fd11e6ba22fa66ae6
LolaMedina <56541025+LolaMedina@users.noreply.github.com>
2020-04-22 17:41:57 +0100

    Updated js task 3

--- JS-Tasks/task3.js
+++ JS-Tasks/task3.js
@@ -8,23 +8,24 @@ const AnimeCheck = num => {
    output.length = 0;
    
    for(let i = 1; i <=num; i++){
-       let word = "";
+       
+       let word = {};
 
        if(i % 2 === 0) {
-           word += "yu";
+           word.two = "yu";
        }
        if(i % 3 === 0) {
-           word += "-gi";
+           word.three = "gi";
        }
        if(i % 5 === 0){
-           word += "-oh";
+           word.five = "oh";
        }
-       if(!word){
-           output.push(i);
-       }
-       else{
-           output.push(word);
-       }
+      const a = Object.values(word);
+      if(a.length === 0){
+          output.push(i)
+      } else{
+          output.push(a.join("-"))
+      }
    }
    return(output);
   
